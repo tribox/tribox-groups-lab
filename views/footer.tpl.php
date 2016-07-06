47a0aac2722a681897bfa62453929a5ae2e9d579
Kotaro Terada <kotaro@tribox.jp>
2016-07-07 00:25:10 +0900

    Update footer note

--- views/footer.tpl.php
+++ views/footer.tpl.php
@@ -5,5 +5,8 @@
 ?>
 
 <footer class="footer">
-  <p>Copyright © 2016</p>
+  <p>
+    Copyright © 2016 <a href="https://github.com/kotarot" target="_blank">Kotaro Terada</a> and <a href="http://tribox.com/" target="_blank">Tribox Inc.</a><br>
+    This is <a href="https://github.com/tribox/tribox-groups-lab" target="_blank">an open source project</a> under GPL v2 license.
+  </p>
 </footer>
